66b2e4c73602998475323a5a7f7075e753061fb3
Jordan <jordandlyon@gmail.com>
2024-06-05 12:01:25 -0300

    Added SFX for player jump with the needed scripts and managers. Know bug: BGM and SFX sharing the same volume control

---
+++ JackInTheBox/Assets/Scripts/Music/SFXPlayer.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXPlayer : MonoBehaviour
+{
+    public static SFXPlayer instance;
+
+    [SerializeField] private AudioSource sfxObject;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+    }
+
+    public void PlayRandomSFX(AudioClip[] audioClip, Transform spawnTransform, float volume)
+    {
+        int rand = Random.Range(0,audioClip.Length);
+
+        AudioSource audioSource = Instantiate(sfxObject, spawnTransform.position, Quaternion.identity);
+
+        audioSource.clip = audioClip[rand];
+        
+        audioSource.volume = volume;
+        
+        audioSource.Play();
+        
+        float clipLength = audioSource.clip.length;
+
+        Destroy(audioSource.gameObject, clipLength);
+    }
+
+    public void PlaySFX(AudioClip audioClip, Transform spawnTransform, float volume)
+    {
+        AudioSource audioSource = Instantiate(sfxObject, spawnTransform.position, Quaternion.identity);
+
+        audioSource.clip = audioClip;
+        audioSource.volume = volume;
+        
+        audioSource.Play();
+        
+        float clipLength = audioSource.clip.length;
+
+        Destroy(audioSource.gameObject, clipLength);
+    }
+}
